63afa44c9ad2e1bd69757db089efc804d65646f8
David Flick <davidflick@Davids-MacBook-Pro.local>
2016-12-12 17:22:40 -0500

    my docs

---
+++ JS/script.js
@@ -0,0 +1,67 @@
+
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    // load streetview
+   var streetStr = $('#street').val();
+   var cityStr = $('#city').val();
+   var address = streetStr +" "+ cityStr;
+   var cityWiki = cityStr.replace(/\s/g, '');
+
+   
+  
+   
+   $greeting.text("So, you want to live at " + address +"?");
+   
+   //Google Street View API Key "AIzaSyBEiC6y3T4fMrwOXdIfqaFfGgVvnO_PiQA"
+   var stViewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location='+address+'';
+   $body.append('<img class="bgimg" src="'+ stViewUrl + '">');
+
+// NYT Article Search API Key: c616321bac5c474cb78b1a1aae61e4c9
+   var nytUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + cityStr + '&sort=newest&api-key=c616321bac5c474cb78b1a1aae61e4c9';
+   $.getJSON(nytUrl, function(data){
+    $nytHeaderElem.text("New York Times articles related to " + cityStr);
+    var articles = data.response.docs;
+    for (var i=0; i<articles.length; i++)
+    $nytElem.append("<li class='article'>"+"<a href='"+articles[i].web_url+"' target='_blank'>"+ articles[i].headline.main +"</a>" +"</li>" + "<br>");
+   }).error(function(){ 
+        $nytHeaderElem.text("New York Times articles could not be loaded!");
+       });
+
+       //Wiki Start
+
+  var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+  var wikiRequestTimeout = setTimeout(function() {
+    $wikiElem.text('Something has gone terribly wrong! Please try again.')
+  }, 8000);
+
+  $.ajax({
+    url: wikiUrl,
+    dataType: 'jsonp',
+    type: 'POST',
+    success: function(data) {
+      for (var i = 0; i < data[1].length; i++) {
+        var articleHeading = data[1][i];
+        var articleUrl = data[3][i];
+        $wikiElem.append('<li> <a href="' + articleUrl + '" target="_blank">' + articleHeading + '</a></li>');
+      }
+      clearTimeout(wikiRequestTimeout);
+    }
+});
+
+
+    // YOUR CODE GOES HERE!
+
+    return false;
+};
+
+$('#form-container').submit(loadData);
